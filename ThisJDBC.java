0b5fb3ad433ad500251e15985990cd599245c214
Anthony Quinones <quinones@cs506-team-38.cs.wisc.edu>
2023-03-04 15:59:08 -0600

    fix

--- ThisJDBC.java
+++ ThisJDBC.java
@@ -2,13 +2,13 @@ import java.sql.*;
 class MysqlCon{
 	public static void main(String args[]){
 		try{
-			Connection dbCxn=DriveManager.getConnection(
+			Connection dbCxn=DriverManager.getConnection(
 						"jdbc:mysql://localhost:33306/ThisAndThat","root","group3"
 					);
 			Statement selectThis=dbCxn.createStatement();
 			ResultSet rsThis=selectThis.executeQuery("select * from This");
 			while(rsThis.next()) {
-				System.out.prinln(rsThis.getInt(1) + " " + rsThis.getString(2) + " " + rsThis.getInt(3) + " " + rsThis.getDate(4) + " " + rsThis.getString(5));
+				System.out.println(rsThis.getInt(1) + " " + rsThis.getString(2) + " " + rsThis.getInt(3) + " " + rsThis.getDate(4) + " " + rsThis.getString(5));
 			}
 			dbCxn.close();
 		}catch(Exception e){ System.out.println(e);}
