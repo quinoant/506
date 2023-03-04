3554129e1e08b6d8143a6ba6a47be20695f2ed39
Anthony Quinones <quinones@cs506-team-38.cs.wisc.edu>
2023-03-04 15:40:14 -0600

    JDBC

---
+++ ThisJDBC.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+class MysqlCon{
+	public static void main(String args[]){
+		try{
+			Connection dbCxn=DriveManager.getConnection(
+						"jdbc:mysql://localhost:33306/ThisAndThat","root","group3"
+					);
+			Statement selectThis=dbCxn.createStatement();
+			ResultSet rsThis=selectThis.executeQuery("select * from This");
+			while(rsThis.next()) {
+				System.out.prinln(rsThis.getInt(1) + " " + rsThis.getString(2) + " " + rsThis.getInt(3) + " " + rsThis.getDate(4) + " " + rsThis.getString(5));
+			}
+			dbCxn.close();
+		}catch(Exception e){ System.out.println(e);}
+	}
+}
