6ca009a225caff956f6408167ef961aac150592d
Anthony Quinones <quinones@cs506-team-38.cs.wisc.edu>
2023-03-04 15:24:15 -0600

    this

---
+++ This.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `ThisAndThat`.`This` ( `This_pk` INT NOT NULL auto_increment, `This_abc` VARCHAR(45) NULL, `This_def` INT NULL, `This_ghi` DATETIME NULL, `This_jkl` ENUM('x', 'y', 'z') NULL, PRIMARY KEY (`This_pk`));
